2b04ff1e1126803713d25a2345da5cb5f5cbd660
VDI\a110082033 <e010659@gmail.com>
2022-11-16 05:58:34 +0800

    bullet timer

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,25 +7,37 @@ public class Bullet : MonoBehaviour
     public float speed = 30f;
     private Rigidbody rb;
     public float atk =100;
+    private float timer;
     
     void Start()
     {
         // 往前飛
         rb = GetComponent<Rigidbody>();
         rb.velocity = transform.forward * speed;
+        
     }
 
     void Update()
     {
-        
+        timer += Time.deltaTime;
+
+        //Debug.Log(timer);
+
+        if (timer >= 3)
+        {
+            Destroy(this.gameObject);
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Enemy")
         {
-         gameObject.SetActive(false);
-         Destroy(gameObject);
+            gameObject.SetActive(false);
+            Destroy(gameObject);
             GameManager.score += 20;
+
+            
         }
+        
     }
 }
